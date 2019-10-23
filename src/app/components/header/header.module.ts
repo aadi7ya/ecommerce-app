c06c933324f10af9613cd1a775c28ffae19b11da
aaditya gandhare <aaditya@flyingpenguins.in>
2019-10-23 09:00:26 +0530

    updated navigation

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header.component';
 
 @NgModule({
@@ -7,7 +8,8 @@ import { HeaderComponent } from './header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent
